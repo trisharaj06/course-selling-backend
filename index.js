baff5a28cf3028bab14a18edd294d348c148b0f0
trisharaj06 <trirox2030@gmail.com>
2025-01-04 16:00:48 +0530

    initial setup and skeleton routes for user

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+
+const app = express()
+
+app.post("/user/signup",(req,res)=>{
+
+})
+
+app.post("/user/signin",(req,res)=>{
+  
+})
+
+app.get("/user/purchases",(req,res)=>{
+  
+})
+
+app.post("/course/purchase",(req,res)=>{
+  
+})
+
+app.post("/courses",(req,res)=>{
+  
+})
+
+app.listen(3000,()=>{
+  console.log('server is running...');
+  
+});
