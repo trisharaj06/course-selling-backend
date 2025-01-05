658aa350eaed2a88548ef274655c3501c2cd273c
trisharaj06 <trirox2030@gmail.com>
2025-01-05 10:23:29 +0530

    add logic to connect with mongoDB

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const express = require("express")
 const userRouter = require("./routes/user");
 const courseRouter = require("./routes/course");
 const adminRouter = require("./routes/admin");
+const { default: mongoose } = require("mongoose");
+require("dotenv").config();
 const app = express()
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>{
+  console.log("MongoDB connected!");
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+})
+.catch((err)=>{
+  console.log("error in connecting with mongoDB",err);
+  
+})
 
 app.use("/user",userRouter);
 app.use("/course",courseRouter)
 app.use("/admin",adminRouter)
-
-app.listen(3000,()=>{
-  console.log('server is running...');
-  
-});
